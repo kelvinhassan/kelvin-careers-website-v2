34370335a5595f0c20073121365e93423946b50a
Hassan <hassan@localhost.localdomain>
2023-08-25 07:06:14 +0300

    jinja templating jobs

--- app.py
+++ app.py
@@ -2,10 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Nairobi, Kenya',
+        'salary': 'Kes. 100,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Nakuru, Kenya',
+        'salary': 'Kes. 150,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'remote',
+
+    },
+    {
+        'id': 1,
+        'title': 'Backend Engineer',
+        'location': 'San Fransisco, USA',
+        'salary': 'USD. 120,000'
+    },
+
+]
+
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='kelvin')
 
 
 if __name__ == "__main__":
