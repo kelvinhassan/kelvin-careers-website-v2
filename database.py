8eb2112dd6ba7eb504dd0027b1feff19ad2266e7
Hassan <hassan@localhost.localdomain>
2023-08-30 08:40:40 +0300

    database engine

--- database.py
+++ database.py
@@ -1,17 +1,9 @@
 from sqlalchemy import create_engine, text
 import os
 
+db_connection_string = "mysql+mysqlconnector://asqx5nahdx2b9yo6dvdy:pscale_pw_Y9GPLPeM3RHjRxesz9hOzvb7NmyC3uEgF0FBRtygckc@aws.connect.psdb.cloud:3306/kelvincareers"
 
-db_connection_string = "mysql+pymysql://luqf3de0ycyamk7eupi9:pscale_pw_aYMFlIsMwakAyGNPXnzPYt7Ic0GRQCfQxzsJB1DN0Pk@aws.connect.psdb.cloud/kelvincareers?charset=utf8mb4"
-
-engine = create_engine(
-    db_connection_string,
-    connect_args={
-        "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-        }
-    }
-)
+engine = create_engine(db_connection_string, echo=True)
 
 
 def load_jobs_from_db():
