200c3c909d1bd9fd17dd9473360abb54bb6a93e0
Hassan <hassan@localhost.localdomain>
2023-08-30 08:12:11 +0300

    databse correction

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
-        jobs = []
-        for row in result.all():
-            jobs.append(dict(row))
-            return jobs
 
 
 @app.route("/")
 def home():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs, company_name='kelvin')
-
-
-@app.route("/jobs")
-def list_jobs():
-    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+import os
+
 
 db_connection_string = "mysql+pymysql://luqf3de0ycyamk7eupi9:pscale_pw_aYMFlIsMwakAyGNPXnzPYt7Ic0GRQCfQxzsJB1DN0Pk@aws.connect.psdb.cloud/kelvincareers?charset=utf8mb4"
 
@@ -10,3 +12,12 @@ engine = create_engine(
         }
     }
 )
+
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row))
+            return jobs
