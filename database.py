13610a395e67fab4f9f39ed366de357ff42fac02
Hassan <hassan@localhost.localdomain>
2023-09-05 07:43:20 +0300

    database connection using pymsql

--- database.py
+++ database.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-DB_HOST = os.getenv("DB_HOST")
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT")
 
 
-db_connection_string = db_connection_string = f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(db_connection_string, echo=True)
+db_connection_string = os.getenv("db_connection_string")
+
+engine = create_engine(
+    db_connection_string,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    })
 
 
 def load_jobs_from_db():
