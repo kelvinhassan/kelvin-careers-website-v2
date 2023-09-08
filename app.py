1d89d53c407e6dad9315810e69f54dd52f9c6dbb
kelvinhassan <ngerajustin@gmail.com>
2023-09-09 00:03:09 +0300

    added extra routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -22,6 +22,13 @@ def show_job(id):
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    return render_template('application_submitted.html', application=data, job=job)
 
 
 if __name__ == "__main__":
